cb0fdaf739d0783afb895c61ea6a287d5f585356
Mona Lisa <daveciaglia@gmail.com>
2021-03-29 20:15:31 +0000

    manipulating List Lab

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -16,7 +16,4 @@ new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
 #print(+ new_list[0], new_list[1], new_list[2], new_list[3], new_list[4], new_list[5])
 
-print ("When I " + new_list[5] + " into IP address " + new_list[3])
-print (" or " + new_list[4] + " I am unable to ping ports")
-#print (str(new_list[0]) + ", " + new_list[1] + ", or " + str(new_list[2]))
-print (new_list[0] , ", " , new_list[1] + ", or " , new_list[2])
+print (f"When I {new_list[5]} into IP address {new_list[3]} or {new_list[4]} I am unable to ping ports, {new_list[0]} , {new_list[1]} , or {new_list[2]}")
