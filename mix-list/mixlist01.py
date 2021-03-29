f1931b4d8fb1fe1d8a84088f191bdcdf228d160f
Mona Lisa <daveciaglia@gmail.com>
2021-03-29 19:34:45 +0000

    List Lab

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -14,4 +14,8 @@ print("The last item in the list (state): " + my_list[2] )
 
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-print(+ new_list[0], new_list[1], new_list[2], new_list[3], new_list[4], new_list[5])
+#print(+ new_list[0], new_list[1], new_list[2], new_list[3], new_list[4], new_list[5])
+
+print ("When I " + new_list[5] + " into IP address " + new_list[3])
+print (" or " + new_list[4] + " I am unable to ping ports")
+print (str(new_list[0]) + ", " + new_list[1] + ", or " + str(new_list[2]))
