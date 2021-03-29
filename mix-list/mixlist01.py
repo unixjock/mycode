79b1836c72cf5ce7d63a831098fdfbbce1478ead
Mona Lisa <daveciaglia@gmail.com>
2021-03-29 19:51:26 +0000

    List Lab

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -18,4 +18,5 @@ new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
 print ("When I " + new_list[5] + " into IP address " + new_list[3])
 print (" or " + new_list[4] + " I am unable to ping ports")
-print (str(new_list[0]) + ", " + new_list[1] + ", or " + str(new_list[2]))
+#print (str(new_list[0]) + ", " + new_list[1] + ", or " + str(new_list[2]))
+print (new_list[0] , ", " , new_list[1] + ", or " , new_list[2])
