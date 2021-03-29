859d214f9e5cc88d812f97f67cf7662f536d8a14
Mona Lisa <daveciaglia@gmail.com>
2021-03-29 19:25:57 +0000

    List Lab

---
+++ input_url.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''THIS IS MY STRING'''
+
+def main():
+    '''MAIN FUNCTION'''
+    urianswer = input("What is the URI you would like to look up?:")
+    print(urianswer)
+
+main()
